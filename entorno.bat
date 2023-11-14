1962b9add9d9be2d4c8745a09f3389949e52bf40
William CWSR <80354334+ChrisWSR@users.noreply.github.com>
2023-11-13 19:36:43 -0700

    Update entorno.bat

--- entorno.bat
+++ entorno.bat
@@ -1,4 +1,8 @@
 @echo off
+
+echo Verificando e instalando Python...
+powershell -Command "& {if (-not(Get-Command python -ErrorAction SilentlyContinue)) {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe' -OutFile 'python-installer.exe'; Start-Process './python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait; Remove-Item 'python-installer.exe'}}"
+
 echo Creando entorno virtual...
 pip install virtualenv
 virtualenv myenv
